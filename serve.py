31ecbaef72af15a57a431b3aa7f57ac366e4128a
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2023-12-08 12:52:32 +1000

    Started Structure

--- serve.py
+++ serve.py
@@ -2,28 +2,42 @@ from livereload import Server, shell
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
+import time
 
 class BuildJupyterBookHandler(FileSystemEventHandler):
     def __init__(self, command):
         self.command = command
+        self.last_run = 0
+        self.debounce_seconds = 2
+
+    def should_rebuild(self):
+        current_time = time.time()
+        if current_time - self.last_run >= self.debounce_seconds:
+            self.last_run = current_time
+            return True
+        return False
 
     def on_any_event(self, event):
-        os.system(self.command)
+        if event.is_directory or not event.src_path.endswith(('.ipynb', '.md')):
+            return
+        if self.should_rebuild():
+            os.system(self.command)
 
 if __name__ == "__main__":
-    path = 'digital_solutions_text'  # Replace with the path to your Jupyter Book
-    build_command = 'jupyter-book build ' + path  # Command to build Jupyter Book
+    source_path = 'digital_solutions_text'  # Source files directory
+    build_output_path = 'digital_solutions_text\_build\html'  # Adjust the path as per your system
+    build_command = 'jupyter-book build ' + source_path
 
     # Set up watchdog observer
     event_handler = BuildJupyterBookHandler(build_command)
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, source_path, recursive=True)
     observer.start()
 
     # Set up livereload server
     server = Server()
-    server.watch(path, shell(build_command))
-    server.serve(root='digital_solutions_text\_build\html')  # Replace with the path to your built book
+    server.watch(source_path, shell(build_command))
+    server.serve(root=build_output_path)
 
     try:
         observer.join()
